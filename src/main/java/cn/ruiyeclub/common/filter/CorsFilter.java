32eeba6c46ff5086ddfaee6148ed72c6b017ed10
chenrui <185048761@qq.com>
2020-07-20 19:43:41 +0800

    跨域的支持

---
+++ src/main/java/cn/ruiyeclub/common/filter/CorsFilter.java
@@ -0,0 +1,29 @@
+package cn.ruiyeclub.common.filter;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 处理跨域问题
+ * @author Ray。
+ * @version 1.0
+ * @date 2020/5/23 15:51
+ */
+@WebFilter(filterName = "CorsFilter ")
+@Configuration
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        chain.doFilter(req, res);
+    }
+}
