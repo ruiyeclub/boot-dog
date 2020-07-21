0a4ec5beb6ab36d6a66f985771a735945f3d8e3e
chenrui <185048761@qq.com>
2020-07-21 15:35:35 +0800

    角色接口的完善

--- src/main/java/cn/ruiyeclub/manage/controller/RoleController.java
+++ src/main/java/cn/ruiyeclub/manage/controller/RoleController.java
@@ -4,6 +4,8 @@ import cn.ruiyeclub.common.annotation.SysLogs;
 import cn.ruiyeclub.common.bean.ResponseCode;
 import cn.ruiyeclub.common.bean.ResponseResult;
 import cn.ruiyeclub.manage.dto.param.FindRoleParam;
+import cn.ruiyeclub.manage.dto.param.RoleAddParam;
+import cn.ruiyeclub.manage.dto.param.RoleUpdateParam;
 import cn.ruiyeclub.manage.service.SysRoleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -12,16 +14,12 @@ import io.swagger.annotations.ApiParam;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 /**
- * @author Licoy
- * @version 2018/4/19/9:41
+ * @author Ray。
+ * @date 2020/4/19 9:41
  */
 @RestController
 @RequestMapping(value = {"/system/role"})
@@ -31,13 +29,40 @@ public class RoleController{
     @Autowired
     private SysRoleService sysRoleService;
 
+    @ApiOperation("分页获取角色数据")
     @RequiresPermissions("system:user:list")
-    @PostMapping(value = {"/list"})
-    @ApiOperation(value = "分页获取角色数据")
+    @PostMapping("/list")
     @SysLogs("分页获取角色数据")
     public ResponseResult get(@RequestBody @Validated @ApiParam(value = "用户获取过滤条件") FindRoleParam findRoleParam){
         return ResponseResult.e(ResponseCode.OK,sysRoleService.listPage(findRoleParam));
     }
 
-    //TODO 修改用户权限
+    @ApiOperation("删除角色")
+    @RequiresPermissions("system:user:remove")
+    @PostMapping("/remove/{id}")
+    @SysLogs("删除角色")
+    public ResponseResult remove(@PathVariable("id") String rid){
+        sysRoleService.removeWithId(rid);
+        return ResponseResult.e(ResponseCode.OK);
+    }
+
+    @ApiOperation("添加角色")
+    @RequiresPermissions("system:user:add")
+    @PostMapping("/add")
+    @SysLogs("添加角色")
+    public ResponseResult add(@RequestBody @Validated RoleAddParam roleAddParam){
+        sysRoleService.add(roleAddParam);
+        return ResponseResult.e(ResponseCode.OK);
+    }
+
+    @ApiOperation("修改角色权限")
+    @RequiresPermissions("system:user:update")
+    @PostMapping("/update/{id}")
+    @SysLogs("修改角色权限")
+    public ResponseResult update(@PathVariable("id") String rid,
+                                 @RequestBody @Validated RoleUpdateParam roleUpdateParam){
+        sysRoleService.update(rid,roleUpdateParam);
+        return ResponseResult.e(ResponseCode.OK);
+    }
+
 }
