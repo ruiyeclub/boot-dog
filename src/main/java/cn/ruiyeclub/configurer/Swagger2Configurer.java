494ceaab4b346dac1c4ff7cb6c84f1bebf0d6c2c
chenrui <185048761@qq.com>
2020-07-21 12:32:46 +0800

    swagger的配置

--- src/main/java/cn/ruiyeclub/configurer/Swagger2Configurer.java
+++ src/main/java/cn/ruiyeclub/configurer/Swagger2Configurer.java
@@ -1,14 +1,21 @@
 package cn.ruiyeclub.configurer;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author licoy.cn
@@ -17,19 +24,44 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2Configurer {
+
+    /** 是否开启swagger */
+    @Value("${swagger.enabled}")
+    private boolean enabled;
+
+    /** 设置请求的统一前缀 */
+    @Value("${swagger.pathMapping}")
+    private String pathMapping;
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                //是否启用Swagger
+                .enable(enabled)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("cn.licoy.wdog.core"))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                /*设置安全模式，swagger可以设置访问token*/
+                .securitySchemes(securitySchemes())
+                .pathMapping(pathMapping);
     }
+
+    /**
+     * 安全模式，这里指定token通过Authorization头请求头传递
+     */
+    private List<ApiKey> securitySchemes(){
+        List<ApiKey> apiKeyList = new ArrayList<ApiKey>();
+        apiKeyList.add(new ApiKey("Authorization", "Authorization", "header"));
+        return apiKeyList;
+    }
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Watch Dog API文档")
-                .description("Watch Dog API文档")
+                .title("Boot-Dog接口文档")
+                .description("更多请咨询服务开发者Ray。")
+                .contact(new Contact("Ray。", "http://www.ruiyeclub.cn", "ruiyeclub@foxmail.com"))
                 .version("1.0")
                 .build();
     }
